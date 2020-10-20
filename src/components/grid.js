70d45b6f81415068a43dd8f1f297df0a460ce600
lee-wood <leemaewood@gmail.com>
2020-10-19 23:58:34 -0400

    building grid component

--- src/components/grid.js
+++ src/components/grid.js
@@ -3,6 +3,17 @@ import Cell from "Cell.js";
 
 function Grid() {
 
+    const rows = 20
+    const cols = 20
+
+    const newBoard = () => {};
+    const BoardGrid = () => {};
+    
+  return(
+      <div>
+          {/*cells go here!*/}
+      </div>
+  )
 }
 
 export default Grid;
