734f55283042955d578ace6966f9eba70c6eef01
lee-wood <leemaewood@gmail.com>
2020-10-26 21:00:11 -0400

    cell automation

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import produce from 'immer';
 import './Grid.css';
 
@@ -13,7 +13,7 @@ const gridStructure = () => {
     return gridRow;
 }
 
-const neighbors = [
+const cellNeighbors = [
     [0, 1],
     [0, -1],
     [1, -1],
@@ -27,11 +27,54 @@ const Grid = () => {
     const [grid, setGrid] = (useState(gridStructure));
     const [running, setRunning] = useState(false);
 
+    const runningRef = useRef(running);
+    runningRef.current = running;
+
     const toggleGrid = ({i, j}) => {
         setGrid(produce(grid, gridCopy => {
             gridCopy[i][j] = grid[i][j] ? 0 : 1;
         }))
     }
+
+    const startButton = () => {
+        setRunning(!running);
+        if(!running) {
+            runningRef.current = true;
+            runGrid()
+        }
+    }
+
+    const runGrid = useCallback(() => {
+        if (!runningRef.current) {
+            return;
+        }
+        
+        setGrid((grid) => {
+            return produce(grid, gridCopy => {
+                for (let i = 0; i < rows; i++) {
+                    for (let j = 0; j < cols; j++) {
+                        let neighbors = 0;
+                        cellNeighbors.forEach(([x, y]) => {
+                            const newI = i + x;
+                            const newJ = j + y;
+                            if(
+                                newI >= 0 && newI < rows && newJ >= 0 && newJ < cols
+                            ) {
+                                neighbors += grid[newI][newJ]
+                            }   
+                        })
+                        
+                        if(neighbors < 2 || neighbors > 3) {
+                            gridCopy[i][j] = 0;
+                        }else if (grid[i][j] === 0 && neighbors === 3) {
+                            gridCopy[i][j] =1;
+                        }
+                    }
+                }
+            })
+        })
+        setTimeout(runGrid, 1000) //one sec
+    }, [])
 
     const toggleCell = ({i, j}) => {
         let toggleBg = 'cell';
@@ -42,13 +85,9 @@ const Grid = () => {
         }
     }
 
-    const startGame = () => {
-        setRunning(!running);
-    }
-
     return(
         <>
-            <button onClick={startGame}>{running ? "Stop" : "Start"}</button>
+            <button onClick={startButton}>{running ? "Stop" : "Start"}</button>
             <div className="grid">
                 {grid.map((gridRow, i) => {
                     return gridRow.map((column, j) => {
